15f2ab5b946321406ebd0a0c8e3e288725c8a84c
JJaist <bobrikoff.max@yandex.ru>
2022-05-16 19:52:06 +0300

    create user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    user_params = params.require(:user).permit(:name, :nickname, :email)
+    User.create(user_params)
+
+    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
   end
 end
