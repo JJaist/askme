d3090a04cb8fa2df4778af44489fedba1a3a7c6d
JJaist <bobrikoff.max@yandex.ru>
2022-05-16 21:21:54 +0300

    initial create user account

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     patch :hide, on: :member
   end
 
-  resource :session, only: %i[new create]
+  resource :session, only: %i[new create destroy]
   resources :users, only: %i[new create]
 end
