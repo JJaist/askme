15cc504c745ae7b6a3570620a1eac930511827e1
JJaist <bobrikoff.max@yandex.ru>
2022-05-18 22:28:18 +0300

    Added User's password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_16_155026) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_192240) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_16_155026) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
