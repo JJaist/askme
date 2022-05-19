b0abed90b6742023c95f5e14f3a1a264fb5f2e2c
JJaist <bobrikoff.max@yandex.ru>
2022-05-19 14:25:24 +0300

    created authentification after user registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user_params = params.require(:session)
-
     user = User.find_by(email: user_params[:email])&.authenticate(user_params[:password])
 
     if user.present?
