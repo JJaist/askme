1e4e663ee3bed780f9e49e9854d28b5e641e7707
JJaist <bobrikoff.max@yandex.ru>
2022-05-20 23:26:06 +0300

    create question answer field

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,7 +4,7 @@ class QuestionsController < ApplicationController
 
   def create
     question_params = params.require(:question).permit(:body, :user_id)
-    @question = Question.new(question_params)
+    @question = Question.create(question_params)
 
     if  @question.save
       redirect_to user_path(@question.user), notice: "Новый вопрос создан"
@@ -15,9 +15,10 @@ class QuestionsController < ApplicationController
   end
 
   def update
+    question_params = params.require(:question).permit(:body, :answer)
     @question.update(question_params)
 
-    redirect_to user_path(@question.user)
+    redirect_to user_path(@question.user), notice: "Изменения сохранены"
   end
 
   def destroy
@@ -52,7 +53,7 @@ class QuestionsController < ApplicationController
 
   private
 
-  def question_params
+  def create_question_params
     params.require(:question).permit(:body, :user_id)
   end
 
