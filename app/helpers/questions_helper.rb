4b7f763bef296de0d5bb97f413d5f422a6550ce0
JJaist <bobrikoff.max@yandex.ru>
2022-05-22 13:34:31 +0300

    fixed task53

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,12 @@
 module QuestionsHelper
+
+  def author_present(author)
+    if author == current_user && current_user.present?
+    "мой вопрос"
+    elsif author.present?
+      link_to "вопрос от #{ display_nickname(author) }", user_path(author)
+    else
+      "вопрос от анонима"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
-
   has_many :questions, dependent: :delete_all
-  has_many :authored_question, class_name: 'question', foreign_key: 'author_id', dependent: :nullify
+  has_many :authored_questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
 
   has_secure_password
 
   before_validation :downcase_nickname
 
   validates :header_color, presence: true, format: { with: /\A#\h{3}{1,2}\z/ }
-
   validates :email, presence: true, uniqueness: true,
     format: { with: /([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})/ }
   validates :nickname, presence: true, uniqueness: true,
